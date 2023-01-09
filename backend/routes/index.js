ac4001b068fed618946d8b706e16cfc3ae24f456
Marlon Santos <santosfmarlon@gmail.com>
2023-01-09 15:16:05 -0500

    before CSRF TOKEN ACCESS IN READING

---
+++ backend/routes/index.js
@@ -0,0 +1,16 @@
+// backend/routes/index.js
+//TEST ROUTER
+const express = require('express');
+const router = express.Router();
+
+router.get('/hello/world', function(req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.send('Hello World!');
+});
+
+
+
+
+
+
+module.exports = router;
