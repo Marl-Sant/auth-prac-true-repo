ffaca77d360c10abc50fb60cb46daf3492dcad9b
Marlon Santos <santosfmarlon@gmail.com>
2023-01-09 15:21:41 -0500

    added CSRF restore route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,27 @@
 // backend/routes/index.js
-//TEST ROUTER
 const express = require('express');
 const router = express.Router();
 
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
+//TEST ROUTE
+// router.get('/hello/world', function(req, res) {
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World!');
+// });
 
+
+//In this route, you are setting a cookie on the response with the name of 
+//XSRF-TOKEN to the value of the req.csrfToken method's return. Then, send 
+//the token as the response for easy retrieval.
+//This route should not be available in production, but it will not be 
+//exclusive to the production application until you implement the frontend 
+//of the application later.
+router.get("/api/csrf/restore", (req, res)=> {
+  const csrfToken = req.csrfToken()
+  res.cookie("XSRF-TOKEN", csrfToken)
+  res.status(200).json({
+    'XSRF-TOKEN': csrfToken
+  })
+})
 
 
 
